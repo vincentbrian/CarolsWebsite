2d6cec138379c10938b40a3da2a8d151d186bdd1
vincentbrian <vincentbrian.design@gmail.com>
2019-01-08 11:12:10 -0500

    smooth scrolling to anchor

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ $('.top').on('click', function (e){
   e.preventDefault();
   $("html, body").animate({scrollTop: 0}, 1000);
 })
+
+$('a[href*=#]:not([href=#])').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+      || location.hostname == this.hostname) {
+
+    var target = $(this.hash);
+    target = target.length ? target : $('[id=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
+// $("a[href^='#']").on("click", function(e) {
+//   e.preventDefault();
+//
+//   return $("html, body").animate({
+//     scrollTop: $(this.hash).offset().top - offset
+//   }, 300);
+// });
